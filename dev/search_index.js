5f87f4c4e43de98cc22d99843b0e165137c8dbcd
Documenter.jl <documenter@juliadocs.github.io>
2023-03-13 22:40:34 +0000

    build based on 010e526

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuliaGendUniv_Optimize","category":"page"},{"location":"#JuliaGendUniv_Optimize","page":"Home","title":"JuliaGendUniv_Optimize","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JuliaGendUniv_Optimize.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuliaGendUniv_Optimize]","category":"page"}]
+}
