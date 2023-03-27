2e5efa61fd4eac7db8f54dfad3645ebd61f7be40
Krishna Bhogaonker <cyclotomiq@gmail.com>
2023-03-27 13:37:01 -0400

    setting up base models for running.

--- src/JuliaGendUniv_Optimize.jl
+++ src/JuliaGendUniv_Optimize.jl
@@ -9,9 +9,10 @@ using Optimization
 include("Genduniv_opt_model_types.jl")
 include("GendUniv_run_model.jl")
 include("GendUniv_optimize_model.jl")
-include("models/basic_model.jl")
+include("models/basic_dde_model.jl")
+include("models/basic_ode_model.jl")
 
-export run_model, optimize_model, estimate_model_with_bayes, BaseModel
+export run_model, optimize_model, estimate_model_with_bayes, BaseModel, DdeModel
 
 
 
