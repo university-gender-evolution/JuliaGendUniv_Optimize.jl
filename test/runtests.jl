b8091c3f38271107cd84ccce3f48f1056efcb6c3
Krishna Bhogaonker <cyclotomiq@gmail.com>
2023-04-07 09:20:18 -0400

    updated tests to see if that helps.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 using JuliaGendUniv_Optimize
-using JuliaGendUniv_Types
 using Test
 using TestItems
 
@@ -14,7 +13,7 @@ end
 
 
 @testitem "[JuliaGendUniv_Optimize] optimization module" begin
-    using JuliaGendUniv_Optimize, Test
+    using Test
 
     @test BasicODEModel() isa JuliaGendUniv_Optimize.AbstractGendUnivModel
     @test BasicDDEModel() isa JuliaGendUniv_Optimize.AbstractGendUnivModel
@@ -22,7 +21,7 @@ end
 end
 
 @testitem "[JuliaGendUniv_Optimize] Run model functions" begin
-    using JuliaGendUniv_Optimize, JuliaGendUniv, Test
+    using JuliaGendUniv, Test
 
     t_preprocess_um_deptindex = preprocess_data("michigan1979to2009_wGender.dta", 
                                 165, UM(); audit_config=NoAudit());
@@ -31,7 +30,7 @@ end
 end
 
 @testitem "[JuliaGendUniv_Optimize] Optimize model functions" begin
-    using JuliaGendUniv_Optimize, Test
+    using Test
 
     @test estimate_model_with_bayes() == 1
     @test optimize_model() == 1
