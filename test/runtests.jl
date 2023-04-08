400ab121d6aa94e9aba5e1107f1a445ce72824b3
Krishna Bhogaonker <cyclotomiq@gmail.com>
2023-04-08 15:51:44 -0400

    updated CI pipeline to develop packages and download test data. (#11)
    * updated CI pipeline to develop packages and download test data.
    
    * refreshed all SSH keys and updated variable names.
    
    * still trying to get the SSH keys to work. I removed the stuff for known_hosts, to see if that works.
    
    * updated all secret keys again. Updated names in CI. Enabled known_hosts.
    
    * going to version 0.8.0. Let's give that a try.
    
    * trying to move the cache operation before adding the repos for dev.
    
    * trying to see if setting environment variable helps to build.
    
    * updated pull for assets. Added environment.
    
    * trying to download asset.
    
    * fixed error in comments.
    
    * fixed error tag.
    
    * adjusting sequence for downloads.
    
    * fixed error in download repo name.
    
    * updated tests to see if that helps.
    
    * fixed test cases, and imports. Also added basic_dde_model optimization.
    
    * update gitignore to ignore manifest and project tomls.
    
    * trying to update LD_LIBRARY_PATH to fix issue with finding R.
    
    * trying to get LD_LIBRARY_PATH to work with R.
    
    * added environment to build julia task.
    
    * setup correct environment variables. added build step for RCall and separated out its install.
    
    * trying to debug CI. Printing path to R.
    
    * searching for libR.so file
    
    * still searching for libR.so file
    
    * still trying to find libR.so.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 using JuliaGendUniv_Optimize
-using JuliaGendUniv_Types
 using Test
 using TestItems
 
@@ -14,15 +13,18 @@ end
 
 
 @testitem "[JuliaGendUniv_Optimize] optimization module" begin
-    using JuliaGendUniv_Optimize, Test
+    using Test
 
-    @test BasicODEModel() isa JuliaGendUniv_Optimize.AbstractGendUnivModel
-    @test BasicDDEModel() isa JuliaGendUniv_Optimize.AbstractGendUnivModel
+    @test JuliaGendUniv_Optimize.BasicODEModel() isa JuliaGendUniv_Optimize.AbstractGendUnivModel
+    @test JuliaGendUniv_Optimize.BasicDDEModel() isa JuliaGendUniv_Optimize.AbstractGendUnivModel
 
 end
 
 @testitem "[JuliaGendUniv_Optimize] Run model functions" begin
-    using JuliaGendUniv_Optimize, JuliaGendUniv, Test
+    using JuliaGendUniv, JuliaGendUniv_Types, Test
+    
+    cd(@__DIR__)
+    @show pwd()
 
     t_preprocess_um_deptindex = preprocess_data("michigan1979to2009_wGender.dta", 
                                 165, UM(); audit_config=NoAudit());
@@ -31,7 +33,7 @@ end
 end
 
 @testitem "[JuliaGendUniv_Optimize] Optimize model functions" begin
-    using JuliaGendUniv_Optimize, Test
+    using Test
 
     @test estimate_model_with_bayes() == 1
     @test optimize_model() == 1
